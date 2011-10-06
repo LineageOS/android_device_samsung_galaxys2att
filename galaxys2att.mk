896662ec70e67c10efdf5ca147652e6db0e9803c
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-06 17:49:18 +0200

    add nfc packages

--- galaxys2att.mk
+++ galaxys2att.mk
@@ -39,7 +39,7 @@ $(call inherit-product-if-exists, device/samsung/c1-common/common.mk)
 
 # apns config file
 PRODUCT_COPY_FILES += \
-        vendor/cyanogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/cyanogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
@@ -56,8 +56,15 @@ PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-        frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# NFC Packages
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
@@ -77,20 +84,20 @@ PRODUCT_PROPERTY_OVERRIDES := \
 # Note that the only such settings should be the ones that are too low-level to
 # be reachable from resources or other mechanisms.
 PRODUCT_PROPERTY_OVERRIDES += \
-       wifi.interface=eth0 \
-       wifi.supplicant_scan_interval=20 \
-       ro.telephony.ril_class=samsung \
-       ro.telephony.sends_barcount=1 \
-       mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
-       dalvik.vm.heapsize=64m \
-       persist.service.usb.setting=0 \
-       dev.sfbootcomplete=0
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=20 \
+    ro.telephony.ril_class=samsung \
+    ro.telephony.sends_barcount=1 \
+    mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
+    dalvik.vm.heapsize=64m \
+    persist.service.usb.setting=0 \
+    dev.sfbootcomplete=0
 
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.com.google.locationfeatures=1 \
-        ro.com.google.networklocation=1
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
 
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
